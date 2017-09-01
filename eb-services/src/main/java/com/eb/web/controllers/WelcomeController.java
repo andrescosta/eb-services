e61a08c201aea3a2ef553d8a8e6e22703fb9a4b5
Andres Costa <andrescosta@Andress-MacBook-Pro.local>
2017-09-01 07:36:30 -0400

    added website

---
+++ eb-services/src/main/java/com/eb/web/controllers/WelcomeController.java
@@ -0,0 +1,36 @@
+
+package com.eb.web.controllers;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	@GetMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("time", new Date());
+
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+			final UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+			model.put("message", userDetails.getUsername());
+		} else {
+			model.put("message", "uu");
+		}
+		return "welcome";
+	}
+
+	@RequestMapping("/foo")
+	public String foo(Map<String, Object> model) {
+		throw new RuntimeException("Foo");
+	}
+
+}
